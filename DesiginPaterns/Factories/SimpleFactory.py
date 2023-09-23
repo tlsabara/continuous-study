e065f7f102feacf207de0c31e8f31364facf6575
Thiago Sabara <thi.sil.sab@gmail.com>
2023-09-23 00:17:38 -0300

    fix(patters): Simple Factory
    missed values

--- DesiginPaterns/Factories/SimpleFactory.py
+++ DesiginPaterns/Factories/SimpleFactory.py
@@ -1,4 +1,4 @@
-from abc import abstractmethod, ABV
+from abc import abstractmethod, ABC
 from enum import Enum, auto
 import uuid
 
@@ -7,12 +7,15 @@ class DeliveryClass(Enum):
     EXPRESS = auto()
 
 class DeliveryOrder(ABC):
-    def __init__(self, sender_address:str=None, reciver_address:str=None, contact:str=None *args, **kwargs) -> None:
+    def __init__(self, sender_address:str=None, reciver_address:str=None, contact:str=None, *args, **kwargs) -> None:
         self.id: str = uuid.uuid4()
         self.sender_address: str = sender_address
         self.reciver_address: str = reciver_address
         self.contact: str = contact
         self._valid: bool = False
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}<{self.id}>"
 
     def validate(self) -> str:
         if self.reciver_address and self.sender_address:
@@ -28,17 +31,30 @@ class DeliveryOrder(ABC):
 class BasicDelivery(DeliveryOrder):
     def start_delivery(self) -> str:
         print("Enviando SMS para o contato")
-        return f"My name is {self.frist_name}, and i'm a student of basic school"
+        return f"Pedido inciado com suicesso"
 
 
 class ExpressDelivery(DeliveryOrder):
     def start_delivery(self) -> str:
         print("Ligando para o ")
-        return f"My name is {self.frist_name}, and i'm a student of high school"
+        return f"Pedido de entrega inciado"
 
 
 class DeliveryFactory:
     @staticmethod
-    def get_student(student_type: Student, frist_name, last_name):
-        if student_type == Student.BASIC_STUDENT:
-            return BasicStudent()
+    def get_delivery(delivery_type: DeliveryClass):
+        if delivery_type == DeliveryClass.BASIC:
+            return BasicDelivery()
+        if delivery_type == DeliveryClass.EXPRESS:
+            return ExpressDelivery()
+
+        else:
+            raise TypeError("DeliveryClass not match.")
+
+if __name__ == "__main__":
+    pedido = DeliveryFactory.get_delivery(DeliveryClass.EXPRESS)
+    print(pedido)
+    pedido = DeliveryFactory.get_delivery(DeliveryClass.BASIC)
+    print(pedido)
+    pedido = DeliveryFactory.get_delivery("Ola")
+    print(pedido)
