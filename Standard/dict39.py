3fcb94bee1a4934b393b7e20f856cf3121bd5d82
Thiago Sabara <thi.sil.sab@gmail.com>
2023-10-13 15:51:40 -0300

    fix: operations on dicts

--- Standard/dict39.py
+++ Standard/dict39.py
@@ -1,9 +1,36 @@
+""" This file was creted to test a simple feature of python 3.9
+
+The | (pipe) operator for dicts
+"""
+
+
 a = {
-"k1": "ovo",
-"k2": "bala",
-"k3": "pedra"
+    "k1": "ovo",
+    "k2": "bala",
+    "k3": "pedra"
+}
+
 b = {
-"k2": "carro",
-"k6": "oculos"
-print(a | b)
+    "k2": "carro",
+    "k6": "oculos"
+}
+
+c ={
+    "k7": "casa",
+    "k9": "brinquedo"
+}
+
+print("DICT A: ", a)
+print("DICT B: ", b)
+
 print()
+print("Operation (a | b): ", a | b)
+print()
+
+print("B before Operation: ", b)
+print("C before Operation: ", c)
+
+b |= c
+print()
+print("B after Operation (b |= c): ", b)
+
