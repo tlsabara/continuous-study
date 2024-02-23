27bac883f0ba510e3f837520772116e9f2f309fb
Sabará <thi.sil.sab@gmail.com>
2024-02-22 23:24:40 -0300

    Increase complexity in turtle graphics proof of concept
    Updated proof of concept script for turtle graphics to enhance the complexity of geometric shapes and color dynamics. The revisions change the number of iterations, the angle of rotation, and the distance moved forward - resulting in more intricate graphics. The hue increment in colorsys is maintained to ensure a vibrant display while exploring the capabilities of Python's turtle module.

--- Libs/turtle/poc_turtle.py
+++ Libs/turtle/poc_turtle.py
@@ -7,14 +7,21 @@ pensize(2)
 
 hue = 0.0
 
-for i in range(300):
+for i in range(600):
     color =colorsys.hsv_to_rgb(hue,1,1)
     pencolor(color)
     hue += 0.005
-    right(i)
-    circle(50,i)
-    forward(i)
-    left(91)
+    right(i/2)
+    circle(i * 0.45, i/2)
+    forward(i/2)
+    left(77+i/2)
+    right(33+i/2)
+    forward(i/2)
+    left(77+i/2)
+    right(33+i/2)
+
+
+
 
 done()
 input()
